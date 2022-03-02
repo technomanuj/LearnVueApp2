6b7a59764fc9be1eaddf0743531f4892e6438a02
Manuj Sharma <technomanuj@hotmail.com>
2022-03-02 15:51:33 +0530

    First commit

---
+++ src/components/AboutComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    
+            <h2 class="greeting">{{greetings}}</h2>
+            <p>{{info}}</p>
+     
+</template>
+<script>
+        import axios from 'axios';
+
+        export default{
+            data(){
+                return{
+                    greetings:"About Page",
+                    info:null
+                }
+            },
+            mounted(){
+                    axios
+                        .get('https://jsonplaceholder.typicode.com/users/2')
+                        .then(response => (this.info = response))
+            }
+        }
+</script>
+<style>
+.greeting {
+  color: red;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,10 @@
+<template>
+    
+            <h2>Home Page</h2>
+    
+</template>
+<script>
+        
+</script>
+<style>
+</style>
