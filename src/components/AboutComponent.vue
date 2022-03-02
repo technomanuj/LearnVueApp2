e4aacc5aa2d0e1f65100b08f775aa696c5b0e170
Manuj Sharma <technomanuj@hotmail.com>
2022-03-02 17:12:07 +0530

    update comment

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -10,7 +10,7 @@
         export default{
             data(){
                 return{
-                    greetings:"About Page",
+                    greetings:"About Vue",
                     info:null
                 }
             },
